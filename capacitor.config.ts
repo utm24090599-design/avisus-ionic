355999f1737abc80b01c15f5a0ffbce7f3f69eb4
Cesar Eduardo <cesar.plyed@gmail.com>
2026-03-03 14:56:56 -0600

    Start Project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Avisus',
+  webDir: 'www'
+};
+
+export default config;
