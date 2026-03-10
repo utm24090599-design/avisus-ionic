bf6501c8b3415920bd20bd0b34be68b015e4f2c5
Yare <utm24090522@utma.edu.mx>
2026-03-10 13:04:03 -0600

    mi parte nueva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, SidebarComponent],
 })
 export class AppComponent {
   constructor() {}
