1fbf670a4e2751f93d2f83c220ccd2ba15a83429
Cesar Eduardo <cesar.plyed@gmail.com>
2026-03-10 13:16:08 -0600

    Fix(register.component.html/.ts, sidebar.component.ts, app.routes.ts ): Encapsuled all into a formGroup, fixed unused imports, redirect home changed to login

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
+  imports: [CommonModule, ReactiveFormsModule]
 })
-export class RegisterComponent  implements OnInit {
+export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  private platformId = inject(PLATFORM_ID);
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  mostrarPassword = false;
+  registerForm: FormGroup;
+  private isBrowser: boolean;
 
-  ngOnInit() {}
+
+  constructor() {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+
+  onSubmit() {
+    this.router.navigate(['login']);
+  }
 
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { IonContent, IonMenu, IonTitle, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonIcon, IonItem, IonLabel, IonList, IonRouterOutlet } from "@ionic/angular/standalone";
+import { IonContent, IonMenu, IonTitle, IonHeader, IonToolbar, IonItem, IonList } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
-  imports: [IonContent, IonMenu, IonTitle, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonIcon, IonItem, IonLabel, IonList, IonRouterOutlet],
+  imports: [IonContent, IonMenu, IonTitle, IonHeader, IonToolbar, IonItem, IonList],
 })
-export class SidebarComponent  implements OnInit {
+export class SidebarComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
