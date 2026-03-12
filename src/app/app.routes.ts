4627bf5e078cf4b9b9e956fc9143f980d0b8760c
joseluis246 <utm24090904@utma.edu.mx>
2026-03-12 11:13:08 -0600

    Edned

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,14 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
+    {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then((m) => m.DashboardComponent),
+  },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
+
 ];
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard', // IMPORTANTE: Este es el nombre para el HTML
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'], // Si usas SCSS, cambia la extensión a .scss
+  imports:[CommonModule]
+})
+export class DashboardComponent {
+  // Datos basados en tu imagen
+  newsItems = [
+    {
+      title: 'polo norte en peligro!!!',
+      imageUrl: 'https://via.placeholder.com/400x200?text=Artico'
+    },
+    {
+      title: 'Noticias Internacionales',
+      imageUrl: 'https://via.placeholder.com/400x200?text=Politica'
+    },
+    {
+      title: '¡¡¡Adictos al empeño en la universidad metropolitana!!!',
+      imageUrl: 'https://via.placeholder.com/400x200?text=Pawn+Shop'
+    }
+  ];
+
+  constructor() { }
+}
